8963c7b878143998fa15c82763dc5e45ea07f2d1
Amandaf-9 <derek607080@gmail.com>
2023-02-02 08:14:50 -0500

    Update documentation

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230129.185945';
+const cacheName = 'chirpy-20230202.081432';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
